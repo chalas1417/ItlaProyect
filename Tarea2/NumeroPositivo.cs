951164f0570f003152dbe70f5aa0175a7df76f58
chalas1417 <chalasleidy1417@gmail.com>
2024-06-23 09:58:09 -0400

    numero positivo

---
+++ Tarea2/NumeroPositivo.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+
+namespace CalcularSueldo.Tarea2
+{
+    public class NumeroPositivo
+    { 
+        public void Positiv()
+        {
+            Console.Write("Ingrese un número positivo de uno o dos dígitos (1..99): ");
+            string input = Console.ReadLine();
+
+           
+            if (int.TryParse(input, out int numero) && numero > 0 && numero < 100)
+            {
+                if (numero < 10)
+                {
+                    Console.WriteLine("El número tiene un dígito.");
+                }
+                else
+                {
+                    Console.WriteLine("El número tiene dos dígitos.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada no válida. Por favor ingrese un número positivo de uno o dos dígitos (1..99).");
+            }
+        }
+    }
+}
+
